d3247a2ec74163f0f0dea54feb7786de49eb5b97
heels <heels@192.168.2.48>
2021-05-25 12:37:25 -0400

    Update CardTrick.java
    added an extra hint

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -18,6 +18,7 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            //assign c to magichHand[i] here
         }
         
         //insert code to ask the user for Card value and suit, create their card
