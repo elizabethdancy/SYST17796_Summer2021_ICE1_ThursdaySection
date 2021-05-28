962b4d405bf93ac5a9125e32e69aa9e0863eacdb
apple <apple@192.168.0.14>
2021-05-27 23:05:27 -0400

    Added certain imports such as the Scanner and the Math packages, Started working on each TODO tasks, and added my name in the modification authors part of each classes.

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -1,5 +1,6 @@
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
+import java.util.Scanner; //Definitely required for the user input process.
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -15,19 +16,51 @@ public class CardTrick
    {
       //TODO: Variable Declarations for the storage of the 7 guessing cards.
       Card[] magicHand = new Card[7];
+      Scanner input = new Scanner(System.in);
 
       //TODO: Store the random values and card suits of the 7 cards.
       for (int i = 0; i < magicHand.length; i++) {
-         Card cards = new Card();
+         int randomValue = ((int) (Math.random() * 13) + 1);
+         int randomSuitNum = ((int) (Math.random() * 4));
 
+         Card cards = new Card();
+         cards.setValue(randomValue);
+         cards.setSuit(cards.SUITS[randomSuitNum]);
+         magicHand[i] = cards;
       }
 
       //TODO: Make a user input for the user to enter their card (their values and
       //their card suit).
+      System.out.print("Enter a value between 1 - 13: ");
+      int cardValue = input.nextInt();
+
+      System.out.print("Enter a suit of cards (Hearts, Diamonds, "
+          + "Spades, Clubs): ");
+      String userSuit = input.next();
+
+
 
       //TODO: Determine the guessing cards that match the user’s card.
+      int equalCards = 0;
+      for (int i = 0; i < magicHand.length; i++) {
+
+         if (cardValue == magicHand[i].getValue()
+             && userSuit.equalsIgnoreCase(magicHand[i].getSuit())) {
+
+
+            equalCards++;
+         }
+      }
 
       //TODO: Determine if the player wins or looses.
+      if (equalCards > 0) {
+         System.out.println("Out of 7 random cards, we found " + equalCards
+             + " card(s) that matches your card, So Congrats, you won!");
+      }
+      else {
+         System.out.println("Out of 7 random cards, neither of the cards"
+             + " matches your card. Unfortunately you lost...");
+      }
    }
 
 }
