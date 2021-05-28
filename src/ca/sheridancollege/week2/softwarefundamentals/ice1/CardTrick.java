14804b96774b754d6559d1bea1355efc09dbc201
Emmett <Emmett@DESKTOP-5F992OQ.cogeco.local>
2021-05-28 18:23:40 -0400

    new branch

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -6,9 +6,14 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifyedBy emmettlaw10 (991628505)
+ * 
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
+   
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
@@ -17,13 +22,46 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random r = new Random() ;
+            int number = r.nextInt(13);
+            int number2 = number + 1;
+            c.setValue(number2);
+            int suit = r.nextInt(4);
+            c.setSuit(Card.SUITS[suit]);
             //assign c to magichHand[i] here
+            magicHand [i] = c;
+            
+            
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+    Scanner scan = new Scanner(System.in); 
+    Card userCard = new Card();
+    System.out.println("Choose the number of your card ( number between 1-13(ace is one, jack is 11, Queen 12, King 13))");
+    userCard.setValue(scan.nextInt());
+    System.out.println("Please choose a number between 0-3 to represent the suit (hearts =0, diamonds = 1, spades = 2, clubs = 3");
+    userCard.setSuit(Card.SUITS[scan.nextInt()]);
+    
+    
+    boolean isCardInHand =false;
+    for (int i = 0; i <= 6; i++) {
+        if (magicHand[i].getValue() == userCard.getValue() && magicHand[i].getSuit().equals(userCard.getSuit()))
+            isCardInHand = true;
+    }
+    // and search magicHand here
+      
+    if (isCardInHand = true) {
+        System.out.println("Your Card was in the magic hand! your card was: " +userCard.getValue() + " of " + userCard.getSuit() );
+    }
+    else {
+        System.out.println("Your Card was NOT in the magic hand :(. your card was: " +userCard.getValue() + " of " + userCard.getSuit() );
+    }
+       //Then report the result here     
+        
+  
+        
+        
         //follow the remainder of the instructions in the ICE 1 doc
     }
     
