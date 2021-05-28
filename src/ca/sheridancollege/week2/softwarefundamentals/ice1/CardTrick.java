b2da68d24142951cd478c0911b106e0294fe180b
apple <apple@192.168.0.14>
2021-05-27 22:27:31 -0400

    New Branch, that's all I can say! It will say the author name's "apple", but that is because I don't know how to change my Mac OS username.

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -1,30 +1,31 @@
-
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author dancye
+ * @modifiedBy Dan Rios 991560038
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            //assign c to magichHand[i] here
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        //follow the remainder of the instructions in the ICE 1 doc
-    }
-    
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         //c.setValue(insert call to random number generator here)
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         //assign c to magichHand[i] here
+      }
+
+      //insert code to ask the user for Card value and suit, create their card
+      // and search magicHand here
+      //Then report the result here
+      //follow the remainder of the instructions in the ICE 1 doc
+   }
+
 }
