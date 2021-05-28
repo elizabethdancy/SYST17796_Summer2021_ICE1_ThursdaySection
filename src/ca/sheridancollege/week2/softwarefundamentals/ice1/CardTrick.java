dc2ae92ce7a5f98e323a8f18006737a1b3b6caf3
nory8 <nory8@Nory-Levi.cogeco.local>
2021-05-28 00:03:58 -0400

    Initial commit

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -1,30 +1,51 @@
-
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author dancye
+ * modified by Nghi Vo 991639672
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            //assign c to magichHand[i] here
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        //follow the remainder of the instructions in the ICE 1 doc
-    }
-    
+import java.util.Random;
+import java.util.Scanner;
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Random rng = new Random();
+      Scanner scanner = new Scanner(System.in);
+      Card[] magicHand = new Card[7];
+      for (int i = 0; i < magicHand.length; i++) {
+         Card card = new Card();
+         //c.setValue(insert call to random number generator)
+         int a = rng.nextInt(13) + 1;
+         card.setValue(a);
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3])
+         int b = rng.nextInt(3);
+         card.setSuit(Card.SUITS[b]);
+         //assign c to magichHand[i] 
+         magicHand[i] = card;
+         System.out.println(card);
+      }
+
+      //insert code to ask the user for Card value and suit, create their card
+      System.out.println("Enter a suit: ");
+      String suit = scanner.nextLine();
+      System.out.print("Enter a value: ");
+      int value = scanner.nextInt();
+// and search magicHand here
+
+      for (int i = 0; i < magicHand.length; i++) {
+         if (value == magicHand[i].getValue() && suit.equalsIgnoreCase(magicHand[i].getSuit())) {
+            System.out.println("Your card was found in the magic hand.");
+            return;
+         }
+      }
+//Then report the result here
+      System.out.println("Your card was not found in the magic hand.");
+//follow the remainder of the instructions in the ICE 1 doc
+   }
 }
