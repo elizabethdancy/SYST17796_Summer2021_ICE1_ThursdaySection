65dd2fcc6aff01ce6566a71f3f03e42a08ddf197
iamya <iamya@LAPTOP-DP8C1GK2>
2021-05-28 22:51:02 -0400

    new branch and edited code

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -6,9 +6,12 @@ package ca.sheridancollege.week2.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Yash Patel 
+ * Student Number: 991387715
  */
+import java.util.*;
+
 public class CardTrick {
-    
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
@@ -17,14 +20,46 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            Random r = new Random(); 
+            int number = r.nextInt(13);
+            int newNum = number + 1;
+            c.setValue(newNum);
+            int suitNum = r.nextInt(4);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[suitNum]);
+
             //assign c to magichHand[i] here
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Value:");
+        int value = in.nextInt();
+        System.out.print("Enter Suit:");
+        int suit = in.nextInt();
+        Card usercard = new Card();
+        usercard.setSuit(Card.SUITS[suit]);
+        usercard.setValue(value);
+        
         // and search magicHand here
+        boolean cardInHand = false;
+        for (int i=0; i<magicHand.length; i++)
+        {
+        	if(usercard.getValue() == magicHand[i].getValue() && usercard.getValue() == magicHand[i].getValue()) {
+        		cardInHand = true;
+        		break;
+        	}
+        	cardInHand = false;
+        }
         //Then report the result here
-        //follow the remainder of the instructions in the ICE 1 doc
+        if(cardInHand == true) {
+        	System.out.print("Card is in the Hand");
+        }
+        else {
+        	System.out.print("Card is not in the Hand");
+        }
+
     }
     
-}
+
