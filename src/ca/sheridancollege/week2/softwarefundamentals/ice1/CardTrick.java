00012526eab74c9ac03dbc9c3722a0d948a35f3d
nadia <nadia@LAPTOP-ORAEQ4EK>
2021-05-29 23:49:31 -0400

    Finished the card gen and magic hand check parts

--- src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/ice1/CardTrick.java
@@ -1,11 +1,15 @@
 
 package ca.sheridancollege.week2.softwarefundamentals.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Nadia Ghazal
  */
 public class CardTrick {
     
@@ -17,7 +21,14 @@ public class CardTrick {
         {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random r = new Random();
+            int number = r.nextInt(13);
+            int newNum = number+1;
+            c.setValue(newNum);
+            int suitNum = r.nextInt(4);
+            c.setSuit(Card.SUITS[suitNum]);
+            System.out.println(c.getValue() + " of: "+ c.getSuit());
+            magicHand[i] = c;
             //assign c to magichHand[i] here
         }
         
@@ -25,6 +36,29 @@ public class CardTrick {
         // and search magicHand here
         //Then report the result here
         //follow the remainder of the instructions in the ICE 1 doc
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a card value between 1 and 13:");      
+        int cardValue = in.nextInt();
+        in.nextLine();
+        System.out.print("Enter a card suit:");
+        String suit = in.nextLine();
+        boolean found = false;
+        for (int i=0; i<magicHand.length; i++)
+        {
+            if(magicHand[i].getValue() == cardValue && suit.toLowerCase().equals(magicHand[i].getSuit().toLowerCase()))
+            {
+                found = true;
+                break;
+            }
+        }
+        
+        if(found) {
+            System.out.println("Your card is in the Magic Hand!!");
+        } else {
+            System.out.println("Your card is NOT in the Magic Hand!");
+        }
+        
+        
     }
     
 }
